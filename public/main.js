55d940dac6bd4e4f6f59a9aeb6111300e4bb036a
Jeremy Glover <jeremyglover33@gmail.com>
2015-11-09 09:22:46 -0600

    Attempting  No. 2

---
+++ public/main.js
@@ -0,0 +1,17 @@
+/*(function() {
+var priceRange = (item) => {
+  return items.filter((item) => {
+    if (item.price) {
+      item.price > 14 & item.price < 18;
+    }
+  });
+};
+
+//var averagePrice = items.filter(between);
+
+console.log(priceRange);
+
+//})();*/
+
+var items[];
+console.log(items);
